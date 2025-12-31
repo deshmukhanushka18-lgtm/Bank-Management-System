2772939a73153f467a62ab8d2df99a91209c4736
deshmukhanushka18-lgtm <deshmukhanushka18@gmail.com>
2026-01-01 00:06:26 +0530

    Add files via upload

---
+++ src/MyProject/Connn.java
@@ -0,0 +1,18 @@
+package MyProject;
+
+import java.sql.*;
+
+public class Connn {
+    Connection connection;
+    Statement statement;
+    public Connn(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
